3185319ec9e5e267cbc574f39bcd20c07d44020e
vijay <vp.gd.msw@gmail.com>
2022-09-30 00:02:35 +0530

    some changes in add.js

--- api/add.js
+++ api/add.js
@@ -13,33 +13,4 @@ const addColumn = (req, res) => {
 };
 
 
-// const addEmployee = (req, res) => {
-//     const employee_id = req.body.employee_id;
-
-//     client.query(`INSERT INTO checkin (employee_id) VALUES ($1) RETURNING * 
-//                 INSERT INTO checkout (employee_id) VALUES ($1) RETURNING *;`, 
-//     [employee_id], 
-//     (error, data) => {
-//         if (error){
-//             throw error;
-//         };
-//         res.status(201).send(`Employee added with ID: ${employee_id}`)
-//     });
-// };
-
-
-// const deleteEmployee = (req, res) => {
-//     const id = parseInt(req.params.id);
-
-//     client.query(`DELETE FROM checkin WHERE employee_id = $1;
-//                 DELETE FROM checkin WHERE employee_id = $1;`, 
-//         [id], 
-//         (error, data) => {
-//         if (error){
-//             throw error;
-//         };
-//         res.status(200).send(`Employee deleted with ID: ${id}`)
-//     });
-// };
-
 module.exports = addColumn;
